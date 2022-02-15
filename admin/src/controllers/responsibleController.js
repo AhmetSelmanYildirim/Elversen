65c31436956a4e86c192ccde636476b9a3e8a20d
AhmetSelmanYildirim <ahmet.selman.yildirimm@gmail.com>
2022-02-15 22:30:25 +0000

    account activated mail has sent

--- admin/src/controllers/responsibleController.js
+++ admin/src/controllers/responsibleController.js
@@ -1,5 +1,6 @@
 const Patient = require("../model/patientModel");
 const Responsible = require("../model/responsibleModel");
+const nodemailer = require("nodemailer")
 
 const getResponsibles = async (req, res, next) => {
 
@@ -16,7 +17,37 @@ const activateResponsible = async (req, res, next) => {
     let data = await JSON.parse(Object.keys(req.body)[0])
 
     try {
-        const result = await Responsible.findOneAndUpdate({email:data.email},{isActive:true})
+        const result = await Responsible.findOneAndUpdate({ email: data.email }, { isActive: true })
+
+        let transporter = nodemailer.createTransport({
+            service: 'Yandex',
+
+            auth: {
+                user: process.env.YANDEX_USER,
+                pass: process.env.YANDEX_PASSWORD
+            }
+        });
+
+        const sentMail = await transporter.sendMail({
+            from: `Elversen SMA Platformu <${process.env.YANDEX_USER}>`,
+            to: data.email,
+            subject: "Belgeleriniz onaylandı",
+            html: `<h1> Merhaba ${result.name},</h1>
+                <h2> Belgeleriniz onaylandı. Giriş yapmak için lütfen bir önceki emaildeki parolanızı kullanınız. </h2>
+                `
+        }, (error, info) => {
+            if (error) {
+                console.log("An error occured: " + error);
+                res.send({ error: `An error occured: ${error.message}` })
+                console.log(info);
+            }
+            else {
+                res.send({ msg: "data received" })
+                console.log("mail has sent");
+                console.log(info);
+                transporter.close();
+            }
+        })
     } catch (error) {
         console.log(error);
     }
@@ -25,7 +56,7 @@ const deactivateResponsible = async (req, res, next) => {
     let data = await JSON.parse(Object.keys(req.body)[0])
 
     try {
-        const result = await Responsible.findOneAndUpdate({email:data.email},{isActive:false})
+        const result = await Responsible.findOneAndUpdate({ email: data.email }, { isActive: false })
     } catch (error) {
         console.log(error);
     }
