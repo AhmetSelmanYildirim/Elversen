958f6839db45a55e7b54772980bc1b2c5d36d8f5
AhmetSelmanYildirim <ahmet.selman.yildirimm@gmail.com>
2022-02-16 19:47:33 +0000

    texts translated to English

--- frontend/client/src/Components/HomepageContent.js
+++ frontend/client/src/Components/HomepageContent.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { strings } from '../Languages/Strings';
 
 
 const HomepageContent = () => {
     return (
         <div className='homepageContent'>
-            <p>Bir el versen! Bir el ver, sen!</p>
-            <p>Her şey olmasa da, bir şey daha güzel olacak.</p>
-            <p>Türkiye’deki tüm SMA’lı çocuklarımız bir arada.</p>
-            <p>Tüm çocuklarımıza daha kolay ulaşın diye.</p>
+            <p>{strings.hText1}</p>
+            <p>{strings.hText2}</p>
+            <p>{strings.hText3}</p>
+            <p>{strings.hText4}</p>
             <Link className="headerItem"
                 to={`/about`}>
-                <p>Daha fazla bilgi için buraya bakabilirsiniz.</p>
+                <p>{strings.hText5}</p>
             </Link>
 
         </div>
--- frontend/client/src/Pages/TermsAndConditions.js
+++ frontend/client/src/Pages/TermsAndConditions.js
@@ -16,13 +16,13 @@ const TermsAndConditions = () => {
             <div className='innerPageContainer' >
                 {strings.termsAndConditions}
                 <div className='aboutUsParagraph'>
-                    <p>……… platformu bir gönüllü organizasyondur.</p>
-                    <p>Amaç, Türkiye’deki SMA’lı çocuklarla yardımseverleri buluşturmaktır.</p>
-                    <p>1.	SMA’lı giriş formu valilik onayında ismi geçen veya SMA’lı çocuğumuzun anne-babasından birisi tarafından doldurulmalıdır. Bu kişi, bundan sonra “sorumlu” olarak anılacaktır.</p>
-                    <p>2.	Sorumlu, verdiği tüm bilgilerin doğruluğundan da sorumludur.</p>
-                    <p>3.	Sorumlu, verdiği tüm bilgilerin doğruluğunu Kabul eder.</p>
-                    <p>4.	Veriler, haftalık olarak güncellenmelidir. Üst üstü iki hafta ve ayrı zamanlar beş hafta güncellenmeyen hesaplar askıya alınır.</p>
-                    <p>5.	Sorumlu, siteye verdiği tüm bilgilerin platformumuz tarafından internette, sosyal medyada, tüm platformlarda ve üçüncü kişilerle paylaşılmasını Kabul eder.</p>
+                    <p>{strings.termsText1}</p>
+                    <p>{strings.termsText2}</p>
+                    <p>{strings.termsText3}</p>
+                    <p>{strings.termsText4}</p>
+                    <p>{strings.termsText5}</p>
+                    <p>{strings.termsText6}</p>
+                    <p>{strings.termsText7}</p>
                 </div>
             </div>
 
