bbb684b440fed55d2588d80ada78936c3ba4c4db
AhmetSelmanYildirim <ahmet.selman.yildirimm@gmail.com>
2022-02-23 11:04:54 +0000

    [clientF] banner added

--- frontend/client/src/Components/HomepageSlider.js
+++ frontend/client/src/Components/HomepageSlider.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Slider = () => {
     return (
-        <div className='slider'  >
-            Banner
+        <div className='sliderArea'  >
+            <img className='slider' src='/img/elversen-banner.jpg' alt='banner'/>
         </div>
     )
 }
